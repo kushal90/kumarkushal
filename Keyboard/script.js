463a779f584dd0981b09a429edc00301e588b5a7
Kushal Kumar <k.kumar13@hotmail.com>
2018-06-12 15:55:33 +0200

    Completed the functionality with some visual effects

---
+++ Keyboard/script.js
@@ -0,0 +1,16 @@
+function playSound(event) {
+    //Use event.code to ignore Caps lock and other languages, etc.
+    let keyId = event.code + "-audio";
+    let key = document.getElementById(keyId.substring(0,4));
+    let audio = document.getElementById(keyId);
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+function removeTransition(e) {
+    e.target.classList.remove("playing");
+}
+const keys = Array.from(document.getElementsByClassName("key key--unpressed"));
+keys.forEach(element => element.addEventListener("transitionend", removeTransition));
+
+window.addEventListener('keydown', playSound);
