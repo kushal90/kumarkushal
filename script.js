3f729ec6e1f98a7b07c66f29e634090db9023d13
Kushal Kumar <k.kumar13@hotmail.com>
2018-07-16 17:48:20 +0200

    Added media queries and some dynamic text.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let intro = "Hi! I am Kushal.";
 
-function writeText(str) {
-	let showing = "";
-	for (let i = 0; i < str.length; i++){
-		showing += str[i];
-		document.getElementById("profile__text").innerHTML = showing;
-		setTimeout(writeText, 1000);
-	}
+let skills = ["Javascript", "Node.js", "BrowserSync", "Gulp"];
+let skillPosition = -1;
+function writeText() {
+	if (skillPosition === skills.length-1)
+		skillPosition = 0;
+	else
+	skillPosition += 1;
+	if (skillPosition === 0)
+		document.getElementById("dynamic-text").innerHTML = skills[skillPosition];
+	document.getElementById("dynamic-text").innerHTML = skills[skillPosition];
 }
-writeText(intro);
 // Animating the burger and opening the navigation menu on small screen devices.
-let burger = document.getElementsByClassName("c-header__burger");
-burger[0].addEventListener("click", () =>
+let burger = document.getElementById("top-burger");
+burger.addEventListener("click", () =>
 {
-	console.log(burger[0].classList);
-	burger[0].classList.toggle("c-header__burger-clicked");
+	console.log(burger.classList);
+	burger.classList.toggle("c-header__burger-clicked");
+
 });
+
+// To position profile pic in the centre.
+function positionProfilePic() {
+		let w = window.innerWidth;
+		let h = window.innerHeight;
+		let profilePic = document.getElementById("profile-picture");
+		let marginLeft = (w-profilePic.getAttribute("height"))
+			- (w-profilePic.getAttribute("height")-document.getElementById("icon-github").width);
+		profilePic.setAttribute("style", `margin-left: ${marginLeft}`);
+
+}
+let interval = setInterval(writeText, 1000);
+positionProfilePic();
