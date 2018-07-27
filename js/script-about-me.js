dd415ed9c8acad7914342425594908ea8e3a9b24
Kushal Kumar <k.kumar13@hotmail.com>
2018-07-27 15:25:43 +0200

    About-me page

---
+++ js/script-about-me.js
@@ -0,0 +1,13 @@
+// function centerId() {
+// 	if (window.innerHeight > window.innerWidth){
+// 		let idImage = document.getElementsByClassName("main-id-image")[0];
+// 		let w = window.innerWidth;
+// 		let idImageWidth = idImage.width;
+// 		// idImage.setAttribute("style", `margin-left:${(w-idImageWidth)/2}px`);
+// 		console.log(w);
+// 		console.log(idImageWidth);
+// 		console.log((w-idImageWidth)/2);
+// 	}
+// }
+//
+// centerId();
