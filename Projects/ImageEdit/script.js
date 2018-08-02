7db92da91d2b3ff8ab984b1aa40c1dec63a14cdd
Kushal Kumar <k.kumar13@hotmail.com>
2018-08-02 15:50:00 +0200

    Styling

--- Projects/ImageEdit/script.js
+++ Projects/ImageEdit/script.js
@@ -23,7 +23,7 @@ sliders.forEach(element => {
         // image.setAttribute("style", `height: ${setWidth/ratio_WtoH}%`);
         let filter = "filter:";
         let grayscale_filter = ` grayscale(${sliders[0].value*10}%)`;
-        let blur_filter = ` blur(${sliders[1].value*10}px)`;
+        let blur_filter = ` blur(${sliders[1].value}px)`;
         let sepia_filter = ` sepia(${sliders[2].value*10}%)`;
         let transparency_filter = ` opacity(${100 - sliders[3].value*10}%)`;
         let filter_string = filter+grayscale_filter+blur_filter+sepia_filter+transparency_filter;
